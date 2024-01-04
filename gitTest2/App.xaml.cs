ee2a35308fa7ca61b28a96ccfafcc6515b26ac6d
macUser <macUser@SPICEPC>
2024-01-04 16:17:13 +0900

    プロジェクト ファイルを追加します。

---
+++ gitTest2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace gitTest2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
